b3ddb07e2b393393d37ffa51b17d6c0224aa3954
kazuto0291 <zutozuto03170291@gmail.com>
2020-02-29 09:03:12 +0900

    devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    # 保存できた場合
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      # 保存できなかった場合
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
